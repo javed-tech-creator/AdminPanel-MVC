0810e80e9e752af53b7ccf40f63d3190b9fc6002
javed-tech-creator <ja8688126@gmail.com>
2025-03-22 23:44:48 +0530

    comment off the multer

--- Backend/Routes/SliderRouter.js
+++ Backend/Routes/SliderRouter.js
@@ -1,26 +1,25 @@
 const {saveSlider,getSlider,deleteSlider,updateSlider} = require('../Controller/SliderController')
 const express = require('express')
 const router = express.Router();
-// const multer = require('multer')
-// const path = require('path')
+const multer = require('multer')
+const path = require('path')
 
-// const storage = multer.diskStorage({
-//   destination:(req,file,cb)=>{
-//     cb(null,path.join(__dirname,'../uploads'))
-//   },
-//   filename:(req,file,cb)=>{
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1E9);
-//     cb(null,file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-//   }
-// })
+const storage = multer.diskStorage({
+  // destination:(req,file,cb)=>{
+  //   cb(null,path.join(__dirname,'../uploads'))
+  // },
+  filename:(req,file,cb)=>{
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random()*1E9);
+    cb(null,file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+})
 
-// const upload = multer({storage})
-// upload.single('image_url') 
+const upload = multer({storage})
 
-router.post('/save',saveSlider)
+router.post('/save',upload.single('image_url'),saveSlider)
 router.get('/get',getSlider)
 router.delete('/delete/:id',deleteSlider)
-router.put('/update/:id',updateSlider)
+router.put('/update/:id',upload.single('image_url'),updateSlider)
 
 
 module.exports = router
